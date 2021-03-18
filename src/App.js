90b6032136b5d5b1b5b0ade981080a40699a6628
Mandy <34908590+picaq@users.noreply.github.com>
2021-03-18 12:30:39 -0700

    modified Users and Events, created DateDay component to pick date

--- src/App.js
+++ src/App.js
@@ -8,45 +8,105 @@ function App() {
       <header className="App-header">
         <h1>React Eventonica</h1>
       </header>
-      <Events />
-      <Users />
+      <main>
+        <Search title="Search"/>
+        <Events />
+        <Users />
+
+        
+      </main>
     </>
     // </div>
   );
 }
 
-function Events() {
-  let title="Event";
+function Events(props) {
+  // let title="Event";
   return (
     <>
-    <h2>Events</h2>
-    <form action="">
+
+    <Form title = "Event">
+      
+
+    </Form>
+    <form>
+
+    <DateDay onChange={() => changeDate()} />
+
+
+    {/* <button value="submit" name="submit"/> */}
+    </form>
+ 
+    </>
+  )
+}
+
+function Users(props) {
+  // let title = "User";
+  return (
+    <>
+
+    <Form title="User" />
+
+    </>
+  )
+}
+
+function Search(props) {
+  return (
+    <>
+    <h2>{props.title}</h2>
+    <form>
+      <input type="text" placeholder="Search for something"/>
+      <button> {props.title} </button>
+    </form>
+    </>
+  )
+}
+
+const Form = (props) => {
+  // let title = "title";
+  return (
+  <>
+    <h2>{props.title}s</h2>
+    <form>
+      {/* <label>
+        {title} Name:
+          <input
+            name="eventName"
+            value={name}
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+          />
+      </label> */}
       <input type="text"/>
-      <button>Add {title}</button>
+      <button>Add {props.title} Name</button>
     </form>
-
-    </>
+  </>
   )
 }
 
-function Users() {
-  let title = "User";
+const DateDay = (props) => {
+  let changeDate = () => {
+
+  }
+  const today = new Date().toISOString().split('T')[0];
   return (
     <>
-    <h2>Users</h2>
-    <form action="">
-    <input type="text"/>  
-    <button>Add {title}</button>    
-    </form>
+    <label for="date">Date:</label>
+      <input 
+        type="date" 
+        id="date" 
+        name="date"
+        value={props.day}
+        min={today}
+
+      />
 
     </>
   )
 }
-
-
-
-
-
 
 
 
