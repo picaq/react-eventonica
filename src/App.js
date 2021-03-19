38094489ea8c3de7bc6c463642928eba15c1d696
Mandy <34908590+picaq@users.noreply.github.com>
2021-03-18 22:18:25 -0700

    copy paste component contents into Events() to allow onClick() and onChange() to work

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 
 const today = new Date().toISOString().split('T')[0];
-
+let space = ".33rem";
+// let eventDate;
 function App() {
 
 
@@ -26,33 +27,91 @@ function App() {
 }
 
 function Events(props) {
-  // let title="Event";
+  let title="Event";
+  const [eventName, setEventName] = useState("");
+
+  useEffect(() => {
+    console.log({eventName})
+  }, [eventName]);
+
+  // const [eventsObject, setEventsObject] = useState({});
+
+  const [eventDate, setDate] = useState(today);
+
+  useEffect(() => {
+    console.log({eventDate})
+  }, [eventDate]);
+
+  let eventsObject = {eventDate, eventName};
+  useEffect(() => {
+    console.log({eventsObject})
+  }, [eventsObject]);
+
+  let sendEventsObject = () => {
+    console.log({eventsObject});
+  }
 
   return (
     <>
 
-    <Form title = "Event">
-      
+    <Title title = {title} />
 
-    </Form>
-    <form>
+    <div>
 
-    <DateDay/>
+    <label htmlFor="date">Date:</label>
+      <input 
+        type="date" 
+        id="date" 
+        name="date"
+        value={eventDate}
+        min={today}
+        onChange={(e) => setDate(e.target.value) }
+      />
+    {/* <NameField 
+      title={title} 
+      value={eventName}
+      placeholder={eventName}
+      onChange={(e) => setEventName(e.target.value)}
+    /> */}
+      <label> {title} Name:&nbsp;
+        <input 
+          type="text"
+          value={eventName}
+          placeholder="eventName"
+      onChange={(e) => setEventName(e.target.value)}
+        />
+      </label>
 
-
+    {/* <AddButton 
+        title={title}
+        onClick={(e) => setEventsObject(e.target,{eventDate, eventName})} 
+    /> */}
+    <button 
+      style={{ display: "block",
+  marginLeft: "auto"}} 
+      onClick={(e) => sendEventsObject(e.target)}>Add {title}
+    </button>
     {/* <button value="submit" name="submit"/> */}
-    </form>
+    </div>
  
     </>
   )
 }
 
 function Users(props) {
-  // let title = "User";
+  let title = "User";
+  let value = "value";
   return (
     <>
 
-    <Form title="User" />
+    <Title title={title} />
+    <form>
+      <NameField title={title} value={value}/>
+      <AddButton 
+        title={title}
+        onClick={(e) => setEventName(e.target.value)} 
+      />
+    </form>
 
     </>
   )
@@ -64,51 +123,63 @@ function Search(props) {
     <h2>{props.title}</h2>
     <form>
       <input type="text" placeholder="Search for something"/>
-      <button> {props.title} </button>
+      <button style={{marginLeft: space}}> {props.title} </button>
     </form>
     </>
   )
 }
 
-const Form = (props) => {
-  // let title = "title";
+const Title = (props) => {
   return (
   <>
     <h2>{props.title}s</h2>
-    <form>
-      <input type="text"/>
-      <button>Add {props.title} Name</button>
-    </form>
   </>
   )
 }
-
-const DateDay = (props) => {
-  // let changeDate = () => {
-  //   console.log(props.day);
-  // }
-
-  const [eventDate, setDate] = useState(today);
-
-  useEffect(() => {
-    console.log({eventDate})
-  }, [eventDate]);
-
+const AddButton = (props) => {
   return (
+  <>
+      <button style={{ display: "block",
+  marginLeft: "auto"}} >Add {props.title}</button>
+  </>
+  )
+}
+const NameField = (props) => {
+  return(
     <>
-    <label htmlFor="date">Date:</label>
-      <input 
-        type="date" 
-        id="date" 
-        name="date"
-        value={eventDate}
-        min={today}
-        onChange={(e) => setDate(e.target.value) }
-      />
-
+      <label> {props.title} Name:&nbsp;
+        <input 
+          type="text"
+          value={props.eventName}
+        />
+      </label>
     </>
   )
 }
+
+// const DateDay = () => {
+
+//   const [eventDate, setDate] = useState(today);
+
+//   useEffect(() => {
+//     console.log({eventDate})
+//   }, [eventDate]);
+
+//   return (
+//     <>
+//     <label htmlFor="date">Date:</label>
+//       <input 
+//         type="date" 
+//         id="date" 
+//         name="date"
+//         value={eventDate}
+//         min={today}
+//         onChange={(e) => setDate(e.target.value) }
+//       />
+
+//     </>
+//   )
+// }
 
 
 
