75cfa0ca1cb69a0d8a65bb27a85f60315cadb0e2
Mandy <34908590+picaq@users.noreply.github.com>
2021-03-22 15:12:13 -0700

    add users routes

--- api/routes/index.js
+++ api/routes/index.js
@@ -6,4 +6,7 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+app.post('/users', (req, res) => {
+  eventonica.addUser(res.body).then(() => res.sendStatus(204));
+});
 module.exports = router;
